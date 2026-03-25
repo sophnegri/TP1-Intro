7617fa5a40b8540b30ca8778f38c6daa6d77e393
sophnegri <sophinegri@gmail.com>
2026-03-25 13:47:28 -0300

    TP1 Intro Bash terminado

--- consolidar.sh
+++ consolidar.sh
@@ -4,13 +4,16 @@ entrada="$HOME/EPNro1/entrada"
 salida="$HOME/EPNro1/salida"
 procesado="$HOME/EPNro1/procesado"
 
+# Limpiar archivo de salida
+> "$salida/$FILENAME.txt"
+
 while true
 do
-	for archivo in $entrada/*.txt;do
-		if [ -f "$archivo" ];then
-			cat "$archivo" >> "$salida/$FILENAME.txt"
-			mv "$archivo" "$procesado"
-		fi
-	done
-sleep 1
-done 
+    for archivo in "$entrada"/*.txt; do
+        [ -e "$archivo" ] || continue
+
+        cat "$archivo" >> "$salida/$FILENAME.txt"
+        mv "$archivo" "$procesado"
+    done
+    sleep 1
+done
